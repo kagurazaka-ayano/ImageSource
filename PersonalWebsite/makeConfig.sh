c233698e4c334a1918e80acfc590ec1216a02899
ayano <kagurazaka-ayano@outlook.com>
2024-01-03 00:42:05 -0500

    update shellscript

--- PersonalWebsite/makeConfig.sh
+++ PersonalWebsite/makeConfig.sh
@@ -1,9 +1,11 @@
 # !/usr/bin/env bash
-files=$(ls . | grep ".png" | tr -d " ")
-root="https://github.com/kagurazaka-ayano/ImageSource/blob/main/PersonalWebsite/"
-touch image.yml
-chmod 777 image.yml
+files=$(ls $PWD/PersonalWebsite | grep ".png" | tr -d " ")
+root="https://github.com/kagurazaka-ayano/ImageSource/blob/$1/PersonalWebsite/"
+out=$PWD/PersonalWebsite/image.yml
+touch $out
+chmod 777 $out
+> $out
 for file in $files
 do
-    echo "$root$file\n" >> image.yml
+    echo "- $root$file" >> $out
 done
