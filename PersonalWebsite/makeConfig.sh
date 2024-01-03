61775f2b1f761d6c3bbdf3e00bbbb4d09bf7bddd
ayano <kagurazaka-ayano@outlook.com>
2024-01-03 01:22:10 -0500

    update workflow and shell

--- PersonalWebsite/makeConfig.sh
+++ PersonalWebsite/makeConfig.sh
@@ -1,11 +1,11 @@
 # !/usr/bin/env bash
-files=$(ls $PWD/PersonalWebsite | grep ".png" | tr -d " ")
+files=$(ls | grep ".png" | tr -d " ")
 root="https://github.com/kagurazaka-ayano/ImageSource/blob/$1/PersonalWebsite/"
-out=$PWD/PersonalWebsite/image.yml
+out=images.yml
 touch $out
 chmod 777 $out
 > $out
 for file in $files
 do
-    echo "- $root$file" >> $out
+    echo "- $root$file?raw=true">>$out
 done
